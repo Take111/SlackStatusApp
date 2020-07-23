a34d711a0cee8a7ff1af1c6f3ff2ada627647853
Take111 <maxmaxastea@gmail.com>
2020-07-23 18:42:44 +0900

    Initial Commit

---
+++ SlackStatusApp/SlackStatusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackStatusAppApp.swift
+//  SlackStatusApp
+//
+//  Created by 竹ノ内愛斗 on 2020/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SlackStatusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
