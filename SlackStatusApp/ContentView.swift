9943ff3b3303aa732c1b94104a0d6f60910e0475
Take111 <maxmaxastea@gmail.com>
2020-07-23 21:58:12 +0900

    wip:

--- SlackStatusApp/ContentView.swift
+++ SlackStatusApp/ContentView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userContents = [UserContent]()
+
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List(userContents, id: \.self) { content in
+                Image(systemName: content.image)
+                VStack(){
+                    Spacer(minLength: 8)
+                    Text(content.name)
+                    Spacer(minLength: 8)
+                }
+            }
+            .navigationTitle("ステータス")
+        }
     }
 }
 
+struct UserContent: Identifiable, Hashable {
+    var id = UUID()
+    var image: String
+    var name: String
+}
+
+let testData: [UserContent] = [
+    UserContent(image: "photo", name: "昼食"),
+    UserContent(image: "photo", name: "夜食")
+]
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(userContents: testData)
     }
 }
---
+++ SlackStatusApp/SlackManager.swift
@@ -0,0 +1,15 @@
+//
+//  SlackManager.swift
+//  SlackStatusApp
+//
+//  Created by 竹ノ内愛斗 on 2020/07/23.
+//
+
+import Foundation
+
+class SlackManager {
+
+    static let token = ""
+
+    
+}
